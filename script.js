80055bfab2b860d7e64da9e5b12f7a3952ecdd1f
CS89 <chrismacaroline@gmail.com>
2023-12-16 17:53:28 +0000

    charcater selection loop started- working in parts- needs refinement

--- script.js
+++ script.js
@@ -21,23 +21,23 @@ const generatedPassword = '';
 // as an array and push each character, then join once you have enough characters
 
 // Function to prompt user for password options
-// function getPasswordOptions() {
-//   // Prompt for password length
-//   // At least 8 characters, no more than 128 characters
-//   // Conditional to check that the number that was entered is in range
-//   // Prompts store data as strings, so need to parse into a number
-//   // If the user's input is out of range, either return out of the function or call the function again
+function getPasswordOptions() {
+  // Prompt for password length
+  // At least 8 characters, no more than 128 characters
+  // Conditional to check that the number that was entered is in range
+  // Prompts store data as strings, so need to parse into a number
+  // If the user's input is out of range, either return out of the function or call the function again
 
-//   // Confirm which character sets to use
-//   // If the user answers false for all, either return out of the function or call the function again
+  // Confirm which character sets to use
+  // If the user answers false for all, either return out of the function or call the function again
   
-//   // Once they select a character set:
-//   // Generate a random character for each selected character set
-//   // Either push selected character sets to a mega-array of all selected characters
-//   // OR you can keep the arrays separate and generate a random number to select the array and another to select the index
+  // Once they select a character set:
+  // Generate a random character for each selected character set
+  // Either push selected character sets to a mega-array of all selected characters
+  // OR you can keep the arrays separate and generate a random number to select the array and another to select the index
   
-//   // Once character sets are selected, move on to generating random characters
-// }
+  // Once character sets are selected, move on to generating random characters
+}
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -71,6 +71,7 @@ var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M',
 // Function to prompt user for password options
 
 
+generatePassword()
 
 
 // Function for getting a random element from an array
@@ -87,25 +88,67 @@ function generatePassword() {
     while(true){
     let length =parseInt(userinputlength)
      
-      if (isNaN(length) || length < 8 || length > 128){
+      if (isNaN(length) || length < 8 || length > 128) {
      userinputlength = prompt("Invalid input. Please choose again")}
           else {
-            prompt(`You have chosen ${length} as the length of your password`)
+            alert(`You have chosen ${length} as the length of your password`)
             return length
           }
-         }
-    
+        }
+      }
+      // getPasswordOptions()
+      
+        // Function to prompt user for password options
+        function getPasswordOptions() {
+              console.log("inside.passwordoptions")
+          const parentArray = {numericCharacters, specialCharacters, upperCasedCharacters, lowerCasedCharacters};
+          let chosenArray =[];
 
-  let userChoicetypechosen = prompt("The options are 1. Numbers  2. Special Characters 3. Capital alphabet letters 4. Small alphabet letters. Please enter an option of 1 to 4 in the box below")
 
-  if (userChoicetypechosen === "1" || userChoicetypechosen === "2" || userChoicetypechosen === "3" || userChoicetypechosen === "4") {
-    alert (`You have chosen ${userChoicetypechosen} as your choice of character type`)
-  }
-  else {
-    prompt("Input Invalid. Please choose again")
-  }
-  return;
-}
+          for (let key in parentArray ) {
+      
+            let chosenArrayoption = prompt(`Would you like ${key} in your password? Please type in 'yes' or 'no' in the box below`)
+              if ((chosenArrayoption).toLowerCase() === "yes" ) {
+              
+                  let randomCharacter = parentArray[key][Math.floor(Math.random()*parentArray[key].length)]
+                  chosenArray.push(randomCharacter);
+                  console.log(chosenArray)
+                  }
+                  // else if ((chosenArrayoption).toLowerCase() === "no") {
+                  //   alert("Ok , Thank you")
+                  // }
+                  else {
+                    prompt("Please choose atleast one character type")
+                  }
+          } 
+        } 
+        getPasswordOptions();
+      
+       
+        // Confirm which character sets to use
+          // If the user answers false for all, either return out of the function or call the function again
+          
+          // Once they select a character set:
+          // Generate a random character for each selected character set
+          // Either push selected character sets to a mega-array of all selected characters
+          // OR you can keep the arrays separate and generate a random number to select the array and another to select the index
+          
+          // Once character sets are selected, move on to generating random characters
+
+  
+   
+//         const userChoicetypechosen = prompt("Would you like numbers in your password? Please type in 'yes' or 'no")
+
+//         if (userChoicetypechosen === "yes") {
+//     alert (`You have chosen numbers in your password`)
+//           }
+//   else if (userChoicetypechosen === "no" ){
+//     prompt("Would you like special characters in your password?")
+//   }
+//   return;
+// }
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
